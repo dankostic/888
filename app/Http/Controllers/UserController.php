8432de5971483ccbb5fb1ebd0119a7f00d7c7259
Asnex <asnex@yahoo.com>
2021-05-28 18:21:53 +0200

    REST API for User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * Class UserController
@@ -14,5 +16,42 @@ class UserController extends Controller
     public function index()
     {
         return User::all();
+    }
+
+    public function show(User $user)
+    {
+        return $user;
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::create(
+            $request->only('first_name', 'last_name', 'email', 'role_id')
+            + ['password' => Hash::make('bassword')]
+        );
+
+        return response($user, Response::HTTP_CREATED);
+
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $user->update(
+            [
+                'first_name' => $request->first_name,
+                'last_name' => $request->last_name,
+                'email' => $request->email,
+            ]
+        );
+        $user->save();
+
+        return response($user, Response::HTTP_ACCEPTED);
+
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
