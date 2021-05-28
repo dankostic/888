08239a4eb1d6ee448c6ccf764e8910eac17fb708
Asnex <asnex@yahoo.com>
2021-05-28 17:35:58 +0200

    User controller, factory, seeder, apiResource

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+/**
+ * Class UserController
+ * @package App\Http\Controllers
+ */
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+}
